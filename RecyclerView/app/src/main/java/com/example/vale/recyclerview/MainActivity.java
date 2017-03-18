7c7dc489d1b88fd4b47bfb99f4aad2fc7507e602
Val <valexx55@gmail.com>
2017-03-18 01:14:08 +0100

    update

--- RecyclerView/app/src/main/java/com/example/vale/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/vale/recyclerview/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.vale.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 
 import java.util.ArrayList;
 
@@ -16,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Libro> datos;
 
+    private AdapterLibros adaptador;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,24 +31,63 @@ public class MainActivity extends AppCompatActivity {
         datos = new ArrayList<Libro>();
         Libro libro_aux = null;
 
-        for(int i=0; i<20; i++)
-        {
-            libro_aux = new Libro("Libro "+ i , " Autor " + i);
+        for (int i = 0; i < 20; i++) {
+            libro_aux = new Libro("Libro " + i, " Autor " + i);
             datos.add(libro_aux);
         }
 
         recView = (RecyclerView) findViewById(R.id.RecView);
-        recView.setHasFixedSize(true);
+        //recView.setHasFixedSize(true);//opcional, si sé que el tamaño no va a cambiar
 
-        AdapterLibros adaptador = new AdapterLibros(datos);
+        adaptador = new AdapterLibros(datos);
 
 
         recView.setAdapter(adaptador);
 
-        recView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        recView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         //recView.setLayoutManager(new GridLayoutManager(this,3));
+        //StaggeredGridLayoutManager para celdas de tamaño variable
+        //recView.setLayoutManager(new StaggeredGridLayoutManager());
+
+
+        //ITEM DECORATOR --> OPCIONAL
+
+        recView.addItemDecoration(
+                new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        //
+        //recView.setItemAnimator(new DefaultItemAnimator());
+
+        registerForContextMenu(recView);
+
+       // recView.setContextClickable(true);
 
     }
 
+    public void aniadir (View v)
+    {
 
+        datos.add(1, new Libro("Nuevo", "Autor X"));
+        adaptador.notifyItemInserted(1);
+    }
+
+    public void eliminar (View v)
+    {
+        datos.remove(1);
+        adaptador.notifyItemRemoved(1);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+       // MenuItem mi1 = menu.add(Menu.NONE, 1, 1, "INSERTAR");
+        MenuItem mi2 = menu.add(Menu.NONE, 2, 2, "BORRAR");
+        //MenuItem mi3 = menu.add(Menu.NONE, 3, 3, "MODIFICAR");
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+        int position = info.position;
+
+        Log.d("MENSAJE", "POS_TOCADA = " + position);
+
+    }
 }
